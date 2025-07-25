fd2f394497ff5f86ebd0f4b1f94832e3ae0faae5
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-07-25 14:59:33 +0200

    fix style and add inputs to configure

--- projects/demo/src/app/flip-pane/flip-pane.component.ts
+++ projects/demo/src/app/flip-pane/flip-pane.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'flip-pane',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './flip-pane.component.html',
   styleUrl: './flip-pane.component.sass'
 })
 export class FlipPaneComponent {
-
+  @Input() textSize: string = '150px'
+  @Input() lineHeight: string = '164px'
 }
