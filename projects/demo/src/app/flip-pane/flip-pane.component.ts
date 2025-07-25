6c1613402d73446aa19db38f2fd514e9dc9cbdce
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-07-25 21:49:49 +0200

    add start, pause and stop

--- projects/demo/src/app/flip-pane/flip-pane.component.ts
+++ projects/demo/src/app/flip-pane/flip-pane.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'flip-pane',
@@ -9,7 +9,76 @@ import { Component, Input } from '@angular/core';
   templateUrl: './flip-pane.component.html',
   styleUrl: './flip-pane.component.sass'
 })
-export class FlipPaneComponent {
+export class FlipPaneComponent implements OnInit, OnDestroy {
+  @ViewChild('name') flipListRef!: ElementRef
   @Input() textSize: string = '150px'
   @Input() lineHeight: string = '164px'
+
+  curr = 0
+  counter = 0
+  timerId: any
+
+  isRunning = true
+
+  ngOnInit(): void {
+    this.startTimer()
+  }
+
+  ngAfterViewInit(): void {
+    this.process(this.counter)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerId)
+  }
+
+  startTimer(): void {
+    this.isRunning = true
+    this.timerId = setInterval(() => {
+      this.counter++
+      this.process(this.counter)
+    }, 1000)
+  }
+
+  pauseTimer(): void {
+    if (!this.isRunning) return;
+
+    clearInterval(this.timerId);
+    this.timerId = null;
+    this.isRunning = false;
+  }
+
+  stopTimer(): void {
+    this.pauseTimer();
+    this.counter = 0;
+    this.process(this.counter);
+  }
+
+  process(value: number): void {
+    const ul = this.flipListRef.nativeElement as HTMLElement;
+    const lis = ul.querySelectorAll('li')
+
+    if (lis.length !== 2) return
+
+    lis.forEach(li => li.classList.remove('before', 'active'));
+
+    const curli = lis[this.curr]
+    curli.classList.add('before')
+
+    this.curr = this.curr === 0 ? 1 : 0
+
+    const nextli = lis[this.curr]
+    nextli.classList.add('active')
+
+    const texts = nextli.querySelectorAll('.flip-text')
+    const formatted = this.addZeros(value % 60, 2)
+
+    texts.forEach(el => {
+      (el as HTMLElement).innerText = formatted
+    })
+  }
+
+  addZeros(val: number, length: number): string {
+    return val.toString().padStart(length, '0')
+  }
 }
